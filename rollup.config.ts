a647c4ecc498bbbe48f01671cf119dfaa0cc8170
HECHT Axel <axel.hct@gmail.com>
2024-03-07 10:03:53 +0100

    ref: remove deprecated deps

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,12 @@
 import remove from "rollup-plugin-delete";
 import {resolve} from "node:path";
-import {terser} from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 export default {
     input: resolve("src", "index.ts"),
     plugins: [
         remove({targets: resolve("lib", "*")}),
-        typescript(),
-        terser()
+        typescript()
     ],
     output: {
         file: resolve("lib", "index.js"),
