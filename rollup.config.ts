c87cf7c4f197c3d125195bd530939f2289b2ea90
HECHT Axel <axel.hct@gmail.com>
2024-03-07 10:42:31 +0100

    fix(style): run eslint

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,15 +1,15 @@
-import remove from "rollup-plugin-delete";
-import {resolve} from "node:path";
-import typescript from "@rollup/plugin-typescript";
+import remove from 'rollup-plugin-delete';
+import {resolve} from 'node:path';
+import typescript from '@rollup/plugin-typescript';
 
 export default {
-    input: resolve("src", "index.ts"),
+    input: resolve('src', 'index.ts'),
     plugins: [
-        remove({targets: resolve("lib", "*")}),
+        remove({targets: resolve('lib', '*')}),
         typescript()
     ],
     output: {
-        file: resolve("lib", "index.js"),
-        format: "cjs"
+        file: resolve('lib', 'index.js'),
+        format: 'cjs'
     }
 }
